3119a492f75ddb9759bd2ad0f50416e0a09f058a
Taha Mohammed BELBATTACH <tbelbattach.prestataire@malakoffmederic.com>
2018-12-25 23:31:29 +0100

    refs #creation du projet back avec spring boot 2.1.1 JPA hibernate Flyway...

---
+++ src/main/java/com/performITS/food/truck/smartfoodservice/SmartFoodServiceApplication.java
@@ -0,0 +1,14 @@
+package com.performITS.food.truck.smartfoodservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFoodServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFoodServiceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/performITS/food/truck/smartfoodservice/SmartFoodServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.performITS.food.truck.smartfoodservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmartFoodServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
